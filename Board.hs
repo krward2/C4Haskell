b56f078ee411b370d80dbacb111fdbff44e91677
krward2 <krward2@miners.utep.edu>
2019-11-28 13:01:21 -0700

    One liners!

--- Board.hs
+++ Board.hs
@@ -6,32 +6,27 @@ module Board where
 	mkPlayer = 1
 	mkOpponent = 2
 	
-	--Consider making isFull bd?
-	isFull::[Int]->Bool
-	isFull col = any (/=0) col
+	isFull::[[Int]]->Bool
+	isFull bd = notElem 0 (concat bd)
 
 	numSlot:: [[Int]]->Int
 	numSlot bd = length bd
 
 	getColumn::[[Int]]->Int->[Int]
-	getColumn bd i
-		|numSlot bd == 1 = head bd
-		|otherwise = head(reverse(take i bd))
+	getColumn bd i = last (take i bd)
 
+	
 	isSlotOpen::[[Int]]->Int->Bool
-	isSlotOpen bd i
-		| i < 1 = False
-		| i <= numSlot bd = not (isFull (getColumn bd i))
+	isSlotOpen bd i = elem 0 (last (take i bd) )
 	
 	insertToken::[Int]->Int->[Int]
 	insertToken (empty:full) p = ([x|x<-full,x==0]) ++ (p:[x|x<-full, x /= 0])
 
 	insertCol::[[Int]]->[Int]->Int->[[Int]]
-	insertCol (h:t) col i
-		|i == 1 = col:t
-		|otherwise = h:(insertCol t col (i-1)) 
+	insertCol (bd) col i = (take (i-1) bd) ++ (col:(drop (i+1) bd))
 
 	dropInSlot::[[Int]]->Int->Int->[[Int]]
 	dropInSlot bd i p
 		|isSlotOpen bd i = insertCol (bd) (insertToken(getColumn bd i) p) (i) 
 		|otherwise = bd
+		
--- Testing.hs
+++ Testing.hs
@@ -1,26 +1,4 @@
 module Testing where
-	--mkEmptyCol::Int->[Int]
-	--mkEmptyCol 0 = []
-	--mkEmptyCol length =
-	--	0 : mkEmptyCol (length-1)
-	
-	--mkEmptyCol::Int->[Int]
-	--mkEmptyCol length
-	--	|length == 1 = [0]
-	--	|otherwise = 0 : mkEmptyCol (length-1)
-	
-	--Improved
-	mkCol::Int->[Int]
-	mkCol height = map (const 0) [1..height]	
-
-	--mkBoard::Int->Int->[[Int]]
-	--mkBoard height width
-	--	|n == 1 = [mkEmptyCol m]
-	--	|otherwise = mkEmptyCol m : (mkBoard (m) (n-1))
-
-	--Improved
-	--mkBoard::Int->Int->[[Int]]
-	--mkBoard height width = map (const (mkCol height)) [1..width]
 	
 	mkBoard::Int->Int->[[Int]]
 	mkBoard height width = replicate width (replicate height 0)
@@ -28,14 +6,13 @@ module Testing where
 	mkPlayer = 1
 	mkOpponent = 2
 
-	--isFull::[Int]->Bool
-	--isFull (h:t)
-	--	|h /= 0 = True
-	--	|otherwise = False
-
 	--Improved
 	isFull::[Int]->Bool
 	isFull col = any (/=0) col
+
+	--Humor
+	isBoardFull::[[Int]]->Bool
+	isBoardFull bd = notElem 0 (concat bd)
 
 	numSlot:: [[Int]]->Int
 	numSlot bd = length bd
@@ -46,16 +23,13 @@ module Testing where
 		|otherwise = head(reverse(take i bd))
 
 	--Can't handle -1
+	--isSlotOpen::[[Int]]->Int->Bool
+	--isSlotOpen bd i
+	--	| i < 1 = False
+	--	| i <= numSlot bd = not (isFull (getColumn bd i))
+	
 	isSlotOpen::[[Int]]->Int->Bool
-	isSlotOpen bd i
-		| i < 1 = False
-		| i <= numSlot bd = not (isFull (getColumn bd i))
-	
-	--board = mkBoard 7 7
-	--isOpen = [isSlotOpen board 1, isSlotOpen board 2, isSlotOpen board 3, isSlotOpen board 4, isSlotOpen board 5, isSlotOpen board 6, isSlotOpen board 7]
-	
-	--splitCol::[Int]->[Int]
-	--splitCol(empty++full) = ()++()
+	isSlotOpen bd i = any (==0) (last (take i bd) )
 
 	insertToken::[Int]->Int->[Int]
 	insertToken (empty:full) p = ([x|x<-full,x==0]) ++ (p:[x|x<-full, x /= 0])
@@ -69,23 +43,6 @@ module Testing where
 	dropInSlot bd i p
 		|isSlotOpen bd i = insertCol (bd) (insertToken(getColumn bd i) p) (i) 
 		|otherwise = bd
-
-	--a = insertToken [0,0,0,0,1] 2
-	--ab = insertToken a 2
-	--b = insertCol [[0],[0],[0]] [1] 1
-	
-	--bd = mkBoard 7 7
-	--bd3 = dropInSlot bd 1 1
-	--bd4 = dropInSlot bd3 1 1
-	
-
-	--testDIS::[[Int]]->Int->Int->Int->[[Int]]
-	--testDIS bd i p n
-	--	|n == 1 = dropInSlot bd i p 
-	--	|otherwise = testDIS bd i p (n-1)
-	
-	--bd2 = testDIS bd 1 1 7
-
 	--       1  2
 	--     \ | /
 	-- _____\|/_____3
