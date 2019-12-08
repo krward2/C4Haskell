f7bf0298543721be2b4d0a1ddd8ecb3f0601765e
krward2 <krward2@miners.utep.edu>
2019-12-08 16:16:36 -0700

    Finished main working on win check

--- Main.hs
+++ Main.hs
@@ -4,14 +4,22 @@ module Main where
 	import Board
 	import System.IO
 
+	nextPlayer p
+		|p == 1 = 2
+		|p == 2 = 1
+		|otherwise = 1
+
 	readSlot bd p = do 
 			i<-getLine
-			case reads i :: [(Int,String)] of
-				[(n, "")] -> putStr(boardToStr playerToChar (dropInSlot bd n p))
+			if (isWonBy bd p) then putStr("Game won")
+			else if (isFull bd) then putStr("Draw")
+			else case reads i :: [(Int,String)] of
+				[(n, "")] -> putStr(boardToStr playerToChar (dropInSlot bd n p))>>
+						readSlot (dropInSlot bd n p) (nextPlayer p)
+						
 				_ -> putStr("invalid input")
 	
-	startBoard = mkBoard 6 7
+
 	main::IO ()
 	main = do
-		readSlot startBoard 1
-		main
+		readSlot (mkBoard 7 6) 1
