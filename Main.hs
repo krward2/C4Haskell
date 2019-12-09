491e60e39a15977eb032b9ceed632e4a7cf330db
krward2 <krward2@miners.utep.edu>
2019-12-08 20:16:53 -0700

    Final Version
    for now

--- Main.hs
+++ Main.hs
@@ -4,22 +4,35 @@ module Main where
 	import Board
 	import System.IO
 
+	--Toggles between players
 	nextPlayer p
 		|p == 1 = 2
 		|p == 2 = 1
 		|otherwise = 1
+	
+	--Gets IO version of index/input
+	getAction i = case reads i :: [(Int, String)] of
+			[(n, "")] -> return (n)
+			_ -> return(-1)
 
+	--Gets IO version of board
+	getBoard bd = return bd
+
+	--Gets IO version of new board
+	getNewBoard bd i p = return(dropInSlot bd i p)
+
+	--The workhorse. Takes user input, decides result of the input, and displays the result
 	readSlot bd p = do 
-			i<-getLine
-			if (isWonBy bd p) then putStr("Game won")
-			else if (isFull bd) then putStr("Draw")
-			else case reads i :: [(Int,String)] of
-				[(n, "")] -> putStr(boardToStr playerToChar (dropInSlot bd n p))>>
-						readSlot (dropInSlot bd n p) (nextPlayer p)
-						
-				_ -> putStr("invalid input")
+			l<-getLine
+			i<-getAction l	
+			newBd<-getNewBoard bd i p
+			if(i<1||i>7) then putStr("Game quit.")
+			else if(isWonBy newBd p) then putStr(boardToStr playerToChar newBd) >> putStr("Game won!")
+			else if(isFull newBd) then putStr(boardToStr playerToChar newBd) >> putStr("Draw!")					
+			else putStr(boardToStr playerToChar newBd ) >> readSlot newBd (nextPlayer p)
 	
 
-	main::IO ()
+	--Prints instructions, and initiates the workhorse.
 	main = do
-		readSlot (mkBoard 7 6) 1
+		putStr("Enter a number 1-7 to drop a token in the corresponding column.\nEnter anything else to exit.\n")
+		readSlot (mkBoard 6 7) 1
